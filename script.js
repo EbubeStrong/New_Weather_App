882cd716ae71d72820a113fdc3468f76ba1d50a0
Abraham Samuel <ebubesammy567@gmail.com>
2025-02-02 01:52:16 +0100

    commit apiCall

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
 const cityInput = document.querySelector(".city-input");
 const searchBtn = document.querySelector(".search-btn");
 
+const apiKey = "ccedc5f26bf12146e4f5d673f45a7492";
+
 searchBtn.addEventListener("click", () => {
   if (cityInput.value.trim() !== "") {
     // console.log(cityInput.value);
-    
-    updateWeatherInfo(cityInput.value)
+
+    updateWeatherInfo(cityInput.value);
 
     cityInput.value = "";
     cityInput.blur();
   }
 });
 
-cityInput.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter' && cityInput.value.trim() !== "") {
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && cityInput.value.trim() !== "") {
     // console.log(cityInput.value);
-    
-    updateWeatherInfo(cityInput.value)
+
+    updateWeatherInfo(cityInput.value);
 
     cityInput.value = "";
     cityInput.blur();
   }
-})
+});
 
-const getFetchData = async (city) => { 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1b7d8e4e9c1b2c0b9c5c8e7c3d5f8b6e`)
-    const data = await response.json()
-    console.log(data)
-    return data
-}
+const getFetchData = async (city, endPoint) => {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}`;
 
-const updateWeatherInfo = (city) => { 
-    constweatherData = getFetchData(city)
-}
+  try {
+    const res = await fetch(apiUrl);
+    if (!res.ok) {
+      throw new Error("Error fetching data");
+    }
+
+    const data = await res.json();
+    console.log(data);
+
+    // return data
+  } catch (error) {
+      console.error('Fetched error', error);
+      return null
+  }
+};
+
+const updateWeatherInfo = async (city) => {
+  const weatherData = getFetchData("weather", city);
+  console.log(weatherData);
+};
